ed32a9048ca87e2474e1e45ec65ef811af94258f
jrgleason <jackiegleason@gmail.com>
2020-03-29 13:15:43 -0400

    Went over sass files and created demo for them. Updated readme to better reflect Sass by using var. Went through with the linter to ensure code is formatted properly

--- index.mjs
+++ index.mjs
@@ -1,61 +1,66 @@
-import { execSync } from 'child_process';
+import {execSync} from 'child_process';
 import commandExists from 'command-exists-promise';
-import path from 'path';
 
-
-
-async function checkDart(){
-    return await commandExists('sass');
+async function checkDart() {
+  return await commandExists('sass');
 }
 
-class KoaSass{
-    constructor(config){
-        if (config) {
-            Object.assign(this, config)
-        }
-        this.loadPath = this.loadPath || "node_modules"
-        // TODO: Check for dart-sass fallback to node-sass
-        checkDart().then(exists=> {
-            if(!exists){
-                console.error("Dart not found!!!!");
-                process.exit();
-            }
-        })
+class KoaSass {
+  constructor(config) {
+    if (config) {
+      Object.assign(this, config);
     }
+    this.encoding = this.encoding || 'utf-8';
+    // TODO: Check for dart-sass fallback to node-sass
+    checkDart().then((exists)=> {
+      if (!exists) {
+        console.error('Dart not found!!!!');
+        process.exit();
+      }
+    });
+  }
 
-    onRequest(ctx, next){
-        if(ctx.path.endsWith('.scss')){
-            // TODO render as scss
-            console.log("The file is scss");
-            console.log(` the context is ${JSON.stringify(ctx.originalUrl)}`)
-            // TODO: rootPath?
-            ctx.body = this.render(ctx.originalUrl);
-        } else if (ctx.path.endsWith('.sass')){
-            // TODO: render as sass
-            console.log("This file is sass");
-            ctx.body = this.render(ctx.path);
-        } else{
-            console.log("Not a Sass File type so moving on");
-            next();
-        }
+  onRequest(ctx, next) {
+    if (
+      ctx.path.endsWith('.scss') ||
+        ctx.path.endsWith('.sass') ||
+        ctx.path.endsWith('.css')
+    ) {
+      console.log('Processing with Dart Sass');
+      ctx.body = this.render(ctx.originalUrl);
+    } else {
+      console.log('Not a Sass File type so moving on');
+      next();
     }
+  }
 
-    render(location){
-        if(!location.endsWith('.scss')) return null;
-        if(this.prefix) location = `/${this.prefix}${location}`
-        if(!this.absolute){
-            location = `${process.cwd()}${location}`;
-        }
-        console.log(`The path for the sass file is ${location}\n Current Directory is ${process.cwd()}`);
-        const a = execSync(`sass ${location} --load-path=${this.loadPath}`);
-        // TODO: Handle all encodings
-        // TODO: Add source Map
-        return a.toString('utf8');
+  render(location) {
+    if (
+      !location.endsWith('.scss') &&
+        !location.endsWith('.sass') &&
+        !location.endsWith('.css')
+    ) return null;
+    if (this.prefix) location = `/${this.prefix}${location}`;
+    if (!this.absolute) {
+      location = `${process.cwd()}${location}`;
     }
+    let command = `sass ${location}`;
+    if (this.output) {
+      command += ` ${this.output}`;
+    }
+    if (this.loadPaths) {
+      this.loadPaths.forEach((path) => command += ` --load-path=${path}`);
+    }
+    console.log(`Running ${command}`);
+    const a = execSync(command);
+    // TODO: Handle all encodings
+    // TODO: Add source Map
+    return a.toString(this.encoding);
+  }
 
-    get koaMiddleware(){
-        return this.onRequest.bind(this);
-    }
+  get koaMiddleware() {
+    return this.onRequest.bind(this);
+  }
 }
 
-export { KoaSass }
+export {KoaSass};
